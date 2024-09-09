457b87a9b765a68d8fc021e17f9f8a331b389926
vkavinmech <vrkavinkumar@gmail.com>
2024-09-09 09:43:39 +0530

    Script Updated

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -4,7 +4,7 @@ describe('Navigate to the url', () => {
     cy.wait(3000);
     cy.get("#email").type("demotest@gmail.com");
     cy.get("#pass").type("demotest");
-    cy.get("#u_0_5_lz").click()
+    cy.get("#u_0_5_f8").click()
     cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
     });
 }); 
