3e973c6da695ea42acdcf4a08166f06a2764637f
vkavinmech <vrkavinkumar@gmail.com>
2024-09-09 09:48:20 +0530

    updated

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -1,10 +1,10 @@
-describe('Navigate to the url', () => {
-    it('Should Enter the text', () => {
+describe('Navigate to the facebook', () => {
+    it('Should perform login with invalid credentials', () => {
     cy.visit('https://www.facebook.com/');
     cy.wait(3000);
     cy.get("#email").type("demotest@gmail.com");
     cy.get("#pass").type("demotest");
-    cy.get("#u_0_5_f8").click()
+    cy.get("#u_0_5_f8").click();
     cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
     });
 }); 
