e4424af78bd1c6398fe5797df0113121cec6bb12
vkavinmech <vrkavinkumar@gmail.com>
2024-09-09 10:09:10 +0530

    Script modified

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -1,10 +1,9 @@
 describe('Navigate to the facebook', () => {
-    it('Should perform login with invalid credentials', () => {
-    cy.visit('https://www.facebook.com/');
+    it('Should perform login', () => {
+    cy.visit('https://www.facebook.com');
     cy.wait(3000);
     cy.get("#email").type("demotest@gmail.com");
     cy.get("#pass").type("demotest");
-    //cy.get("[name='login']").click();
-    //cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
+    cy.get('[data-testid="royal_login_button"]').click();
     });
 }); 
