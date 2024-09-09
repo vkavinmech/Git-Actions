969e6b65fe7bf4a935e57aaa7330d085ed12798b
vkavinmech <vrkavinkumar@gmail.com>
2024-09-09 10:00:43 +0530

    changed

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -4,7 +4,7 @@ describe('Navigate to the facebook', () => {
     cy.wait(3000);
     cy.get("#email").type("demotest@gmail.com");
     cy.get("#pass").type("demotest");
-    cy.get("[name='login']").click();
-    cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
+    //cy.get("[name='login']").click();
+    //cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
     });
 }); 
