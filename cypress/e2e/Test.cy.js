af2acaf488f7e0b32b8e599f13cee8b675ba4e27
vkavinmech <vrkavinkumar@gmail.com>
2024-09-09 09:37:16 +0530

    demo script updated

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -2,8 +2,9 @@ describe('Navigate to the url', () => {
     it('Should Enter the text', () => {
     cy.visit('https://www.facebook.com/');
     cy.wait(3000);
-    cy.get('#email').type("demotest@gmail.com");
-    cy.get('#passContainer').type('demotest');
-    cy.get('#u_0_5_lz').click().should('have.text',"The password that you've entered is incorrect. ")
+    cy.get("#email").type("demotest@gmail.com");
+    cy.get("#passContainer").type("demotest");
+    cy.get("#u_0_5_lz").click()
+    cy.get("._9ay7").should('have.value',"The password that you've entered is incorrect. ")
     });
 }); 
--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,8 +1,9 @@
-describe('Navigate to the url', () => {
+describe('Navigate to the Google url', () => {
   it('Should Enter the text', () => {
   cy.visit('https://www.google.com/');
   cy.wait(3000);
+  cy.title().should('eq','Google');
   cy.get('#APjFqb').type("Automation");
-  cy.title().should('have.text', 'Google');
+  
       });
    }); 
