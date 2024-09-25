40e1b22d3abca4d136462c255e7a77d00fba69b2
vkavinmech <vrkavinkumar@gmail.com>
2024-09-25 11:14:56 +0530

    config file updated

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+     /* on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          // Path to your Chrome user data directory
+          const userDataDir = 'C:\\Users\\Dell\\AppData\\Local\\Google\\Chrome\\User Data'; // Update this path to your actual directory
+          const profileDirectory = 'Profile 9'; // Replace with your actual profile directory
+
+          // Add Chrome launch arguments
+          launchOptions.args.push(`--user-data-dir=${userDataDir}`);
+          launchOptions.args.push(`--profile-directory=${profileDirectory}`);
+          launchOptions.args.push('--no-sandbox');
+          launchOptions.args.push('--disable-dev-shm-usage');
+          
+          return launchOptions;
+        }
+      }); */
     },
   },
 });
